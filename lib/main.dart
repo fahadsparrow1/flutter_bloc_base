070f29096726bb41d5fe3b15982e69238eddb97d
Fahad <snaptownios@sparksupport.com>
2024-12-01 12:35:57 +0530

    dependency injusction using getit completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,16 @@
-import 'package:blog_learn_2/feature/auth/data/data_sources/auth_remote_data_source.dart';
-import 'package:blog_learn_2/feature/auth/data/repository/auth_repository_impl.dart';
-import 'package:blog_learn_2/feature/auth/domain/usecases/user_signup_usecase.dart';
 import 'package:blog_learn_2/feature/auth/presentation/bloc/auth_bloc.dart';
+import 'package:blog_learn_2/init_dependencies.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
-
-import 'core/secrets/superbase_secrets.dart';
 import 'core/theme/theme.dart';
 import 'feature/auth/presentation/pages/login_page.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
-  final supabase = await Supabase.initialize(
-      url: SuperBaseSecrets.supabaseUrl,
-      anonKey: SuperBaseSecrets.supabaseAnonKey
-  );
+  await initDependencies();
   runApp( MultiBlocProvider(
     providers: [
-      BlocProvider(create: (_) => AuthBloc(
-          userSignup: UserSignup(AuthRepositoryImpl(AuthRemoteDataSourceImpl(supabase.client)))
-      ))
+      BlocProvider(create: (_) => serviceLocator<AuthBloc>())
     ],
     child: MyApp()
   ),);
