802a4fcc1695e8b8d982b63ca990b5082cdcc5ac
Fahad <snaptownios@sparksupport.com>
2024-12-06 11:14:17 +0530

    modeland entity added 2

---
+++ lib/feature/auth/data/models/user_model.dart
@@ -0,0 +1,18 @@
+
+import '../../domain/entity/user.dart';
+
+class UserModel extends User {
+  UserModel({
+    required super.email,
+    required super.id,
+    required super.name
+  });
+  
+  factory UserModel.fromJson(Map<String, dynamic> map) {
+    return UserModel(
+        email: map['email'] ?? '',
+        id: map['id'] ?? '',
+        name: map['name'] ?? '',
+    );
+  }
+}
